2531e1a9f0dd97fa3204a69ed6340befa2337aec
soyoon <87157589+xoy8n@users.noreply.github.com>
2022-06-02 10:56:44 +0900

    to make other branch

---
+++ movie/src/App.js
@@ -0,0 +1,12 @@
+/*eslint-disable*/
+
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [따봉, c] = useState(0);
+
+  return <div className="App"></div>;
+}
+
+export default App;
